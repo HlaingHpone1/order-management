f64e85c171c96c70e9d37585db6450f8f1ab58ea
Hlaing Hpone <hlainghpone1@gmail.com>
2026-01-12 16:16:02 +0630

    chore: add project step up
    - add prettier for code formatting
    - husky is for use prettier formatting code when commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettierRecommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/ban-ts-comment": "warn",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
