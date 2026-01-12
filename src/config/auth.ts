b514e8a6c24e8cbf5b6bb4823fa4acedec7d04dd
Hlaing Hpone <hlainghpone1@gmail.com>
2026-01-13 00:04:57 +0630

    feat: (better auth) implement better auth

---
+++ src/config/auth.ts
@@ -0,0 +1,38 @@
+import { betterAuth } from "better-auth/minimal";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { bearer } from "better-auth/plugins";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+
+export const prisma = new PrismaClient({ adapter });
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+  },
+  user: {},
+  experimental: { joins: true },
+  session: {
+    disableSessionRefresh: false,
+    cookieCache: {
+      enabled: false,
+      maxAge: 7 * 24 * 60 * 60, // 7 days cache duration
+      strategy: "jwe",
+      refreshCache: true,
+    },
+    account: {
+      storeStateStrategy: "cookie",
+      storeAccountCookie: true, // Store account data after OAuth flow in a cookie (useful for database-less flows)
+    },
+  },
+  plugins: [bearer()],
+});
